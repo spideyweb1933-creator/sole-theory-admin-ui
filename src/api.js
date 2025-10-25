5322253116bd5931f9e4269ac5565c364c232168
spideyweb1933-creator <spideyweb1933@gmail.com>
2025-10-25 17:36:01 +0300

    api.js

--- src/api.js
+++ src/api.js
@@ -1,2 +1,9 @@
 const API_BASE = 'https://sole-theory-server-backend-9.onrender.com';
+const API_BASE = 'https://sole-theory-server-backend-9.onrender.com';
+
+export const LOGIN_URL    = `${API_BASE}/api/auth/login`;
+export const PRODUCTS_URL = `${API_BASE}/api/products`;
+export const UPLOAD_URL   = `${API_BASE}/api/upload`;
+
+export default API_BASE;
 
